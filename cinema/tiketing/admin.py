aa72f72e4ea347e42646728d12a0ecbdfb1d295d
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-05-13 00:09:19 +0430

    add description's fields to admin area

--- cinema/tiketing/admin.py
+++ cinema/tiketing/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from tiketing.models import Movie, Cinema, Sanse
 
-admin.site.register(Movie)
-admin.site.register(Cinema)
-admin.site.register(Sanse)
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'director', 'length', 'year', 'poster']
+
+
+admin.site.register(Movie, MovieAdmin)
+
+
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ['name', 'city', 'capacity', 'phone', 'address', 'image', 'cinema_code']
+
+
+admin.site.register(Cinema, CinemaAdmin)
+
+
+class SanseAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'cinema', 'start_time', 'price', 'salable_seats', 'free_seat', 'id']
+
+
+admin.site.register(Sanse, SanseAdmin)
